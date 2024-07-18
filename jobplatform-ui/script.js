48e9a53415de88872ba8989a95519db9a63d4fbb
marykasp <kasparian.mary@gmail.com>
2024-07-18 16:58:52 -0500

    test: add name of checkbox to an array if checked

--- jobplatform-ui/script.js
+++ jobplatform-ui/script.js
@@ -1,4 +1,7 @@
 let checkboxes = document.querySelectorAll(".checkbox input");
+let valueList = document.getElementById("value-list");
+let listArray = [];
+let text = "<span>You have selected: </span>";
 
 checkboxes.forEach((c) => {
   c.addEventListener("change", () => {
@@ -6,7 +9,11 @@ checkboxes.forEach((c) => {
     const label = document.querySelector(`label[for=${c.name}]`);
 
     if (c.checked) {
+      // change label color
       label.style.color = "#1A1E20";
+      listArray.push(c.name);
+      valueList.innerHTML = text + listArray.join("/");
+      // filter the job cards
     } else {
       label.style.color = "#808487";
     }
