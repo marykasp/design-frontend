1426b81927e6dd2764048277e2ae00d8fff82859
marykasp <kasparian.mary@gmail.com>
2024-10-02 16:40:01 -0500

    feat: start javascript for the rotate button listener

---
+++ effects/location-slider/script.js
@@ -0,0 +1,30 @@
+const rotateBtn = document.querySelector(".rotate-btn");
+const sliders = document.querySelectorAll(".bg-slide");
+const slides = Array.from(sliders);
+const totalSlides = sliders.length;
+let index = 0;
+
+rotateBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    if (slide.classList.contains("active")) {
+      slide.classList.add("after-active");
+    } else {
+      slide.classList.remove("after-active");
+    }
+  });
+  // slides.forEach((slide) => {
+  //   // remove the active class
+  //   slide.classList.remove("active");
+  // });
+
+  // remove the active class from
+  slides[index].classList.remove("active");
+  index++;
+
+  // reset index to 0 if at end of list
+  if (index == totalSlides) {
+    index = 0;
+  }
+
+  slides[index].classList.add("active");
+});
